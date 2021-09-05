f04a6ae86bca5950e2455ae6828e393b519653f6
Mohammed  Elmir <mohammed.elmir>
2021-09-05 12:17:40 +0000

    navbar component created

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import "./App.css"
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
+import Navbar from "./components/layout/Navbar.js"
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <Navbar/>
+    </Router>
   )
 }
 
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import {Link} from "react-router-dom"
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="container">
+                <div className="nav-content">
+                    <h1 className="nav-logo">
+                    <Link to="/">
+                        <a href="!#">Student Hub</a>
+                    </Link>
+                </h1>
+                <ul className="nav-links">
+                    <li>
+                    <Link to="/">
+                        <a href="!#">About</a>
+                    </Link>
+                    </li>
+                    <li>
+                    <Link to="/">
+                        <a href="!#">Sign Up</a>
+                    </Link>
+                    </li>
+                    <li>
+                    <Link to="/">
+                        <a href="!#">Login</a>
+                    </Link>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
